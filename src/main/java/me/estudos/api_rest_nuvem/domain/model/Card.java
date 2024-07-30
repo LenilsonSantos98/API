0815f9089f78384ea6d50266c64bf66ecef07f7f
rodrigo <lenilsonrodrigo123@gmail.com>
2024-07-30 11:06:15 -0300

    Atributos inseridos nas demais classes do model(account, card, feature, news).

--- src/main/java/me/estudos/api_rest_nuvem/domain/model/Card.java
+++ src/main/java/me/estudos/api_rest_nuvem/domain/model/Card.java
@@ -1,5 +1,31 @@
 package me.estudos.api_rest_nuvem.domain.model;
 
+import java.math.BigDecimal;
+
 public class Card {
     
+    private Long id;
+    private String number;
+    private BigDecimal limit;
+    
+    //Getters and Setters
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNumber() {
+        return number;
+    }
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    public BigDecimal getLimit() {
+        return limit;
+    }
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    } 
 }
