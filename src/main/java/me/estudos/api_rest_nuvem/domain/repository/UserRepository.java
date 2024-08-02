81501910f826607a0b99daab179bfdcd3a9c9026
rodrigo <lenilsonrodrigo123@gmail.com>
2024-08-02 11:04:45 -0300

    explicitando O repositório, configurando o h2-datasource e adicionando a dependencia hikari

--- src/main/java/me/estudos/api_rest_nuvem/domain/repository/UserRepository.java
+++ src/main/java/me/estudos/api_rest_nuvem/domain/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package me.estudos.api_rest_nuvem.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import me.estudos.api_rest_nuvem.domain.model.User;
 
+@Repository
 public interface UserRepository extends  JpaRepository<User, Long> {
     
 }
